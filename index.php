b16d8fac0dd3a009123dd02aacbb14559dcd2f6e
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-09-02 21:16:15 +0200

    Exercise Completed

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
 /*"Snack 1
 Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60"*/
 
 $basketMatch = [
-
   "match1" => [
     "homeTeam" => "Olimpia Milano",
     "homeTeamScore" => "55",
@@ -30,19 +28,17 @@ $basketMatch = [
 
 foreach ($basketMatch as $match) {
 
-      $homeTeam = $match['homeTeam'];
-      $homeTeamScore = $match["homeTeamScore"];
-      $visitingTeam = $match["visitingTeam"];
-      $visitingTeamScore= $match["visitingTeamScore"];
+  $homeTeam = $match['homeTeam'];
+  $homeTeamScore = $match["homeTeamScore"];
+  $visitingTeam = $match["visitingTeam"];
+  $visitingTeamScore= $match["visitingTeamScore"];
 
-      echo $homeTeam ." - " . $visitingTeam . " | " . $homeTeamScore . "-" . $visitingTeamScore . "<br>";
+  echo $homeTeam ." - " . $visitingTeam . " | " . $homeTeamScore . "-" . $visitingTeamScore . "<br>";
 }
 
  ?>
 
-
 <?php
-
 // Snack 2
 // Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
 
@@ -62,3 +58,28 @@ if($nameLength>3 && $dotPos > 0 && $netPos > 0 && is_nan($age)==false){
 };
 
  ?>
+
+ <?php
+ // Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
+
+ $i = 0;
+ $duplicate = false;
+ $newArray=[];
+
+ while( $duplicate == false){
+   $varNum = rand(1,80);
+
+   if(count($newArray) <= 15){
+     if (in_array($varNum, $newArray)==false) {
+       $newArray[] = $varNum;
+     }
+   }
+
+   else{ $duplicate = true; }
+   $i++;
+ };
+
+ echo "<br>";
+ print_r($newArray);
+
+  ?>
